aef5912d1e2e8f259c6db437554a9a71e3dd0b99
Jose Vargas <arnoldovargas1981@gmail.com>
2022-03-18 15:47:03 -0300

    terminado

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const cors = require("cors");
 const app = express();
 app.use(cors());
- 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
